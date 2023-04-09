cf4baadac9a75a9f40e1be5146cbe44586f88edc
thiblehm12 <thiblehm12@gmail.com>
2023-04-09 09:43:30 +0200

    ui for all articles done

--- src/pages/components/items.tsx
+++ src/pages/components/items.tsx
@@ -1,34 +1,35 @@
-import styles from '@/styles/article.module.css'
+import styles from '@/styles/items.module.css'
 import { useState } from 'react';
 import Image from 'next/image'
 import imgTest from '../../images/imageTest.jpeg'
 import { useCtx } from '../auth/ctx';
 const Items = () => {
-    const [value, setValue] = useState('')
-    const { setItems, items } = useCtx()
+    const { items } = useCtx()
     return (
         <div className={styles.container}>
             <h2 className={styles.title}>📦 Items List</h2>
-            <div className={styles.articles}>
-                {/* todo: foreach articles */}
-                {items.map((item) => (
-                    <div key={item.reference}>
-                        <div className={styles.article}>
-                            <div className={styles.top}>
-                                <div className={styles.imgContainer}>
-                                    <Image src={imgTest} alt="Logo" fill />
+            <div className={styles.test}>
+
+                <div className={styles.articles} style={{ justifyContent: items.length < 4 ? 'flex-start' : 'space-around' }}>
+                    {items.map((item) => (
+                        <div key={item.reference}>
+                            <div className={styles.article}>
+                                <div className={styles.top}>
+                                    <div className={styles.imgContainer}>
+                                        <Image src={imgTest} alt="Logo" fill style={{ zIndex: 1 }} />
+                                    </div>
+                                </div>
+                                <div className={styles.bottom}>
+                                    <div className={styles.name}>{item.name}</div>
+                                    <div className={styles.ref}>Ref: #{item.reference}</div>
+                                    <div className={styles.ref}>{item.company}</div>
+                                    <div className={styles.details}>Details</div>
                                 </div>
                             </div>
-                            <div className={styles.bottom}>
-                                <div className={styles.name}>{item.name}</div>
-                                <div className={styles.ref}>Ref: #{item.reference}</div>
-                                <div className={styles.ref}>{item.company}</div>
-                                <div className={styles.details}>Details</div>
-                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
 
+                </div>
             </div>
         </div>
     );
